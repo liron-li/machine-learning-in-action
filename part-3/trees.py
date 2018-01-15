ac25031ae967518a08130bf6b18d4fa98bbac87e
liron.li <liron.li@outlook.com>
2018-01-15 23:45:49 +0800

    3-5

--- part-3/trees.py
+++ part-3/trees.py
@@ -1,5 +1,6 @@
 import math
 import operator
+import matplotlib.pyplot as plt
 
 
 def calc_shannon_ent(data_set):
@@ -116,7 +117,26 @@ def create_tree(data_set, labels):
     return my_tree
 
 
+def plot_node(node_txt, center_pt, parent_pt, node_type):
+    arrow_args = dict(arrowstyle='<-')
+    create_plot.ax1.annotate(node_txt, xy=parent_pt, xycoords='axes fraction', xytext=center_pt,
+                           textcoords='axes fraction', va='center', ha='center', bbox=node_type,
+                           arrowprops=arrow_args)
+
+
+def create_plot():
+    decision_node = dict(boxstyle='sawtooth', fc='0.8')
+    leaf_node = dict(boxstyle='round4', fc='0.8')
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    create_plot.ax1 = plt.subplot(111, frameon=False)
+    plot_node("决策节点", (0.5, 0.1), (0.1, 0.5), decision_node)
+    plot_node("叶节点", (0.8, 0.1), (0.3, 0.8), leaf_node)
+    plt.show()
+
+
 if __name__ == '__main__':
     data_set, labels = create_data_set()
     my_tree = create_tree(data_set, labels)
     print(my_tree)
+    create_plot()
